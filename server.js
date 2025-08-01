9b30d21e13d2baf6b18b11a672f4d29355a7725b
syazwanhosen <syazwanhosen@gmail.com>
2025-08-01 15:53:44 +0800

    check if service account file is exist

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 // server.js
 import express from 'express';
 import serverless from 'serverless-http'; // Wrap express app
+import path from 'path';
+import fs from 'fs';
 
 import votesHandler from './api/votes.js';
 import voteHandler from './api/vote.js';
@@ -10,6 +12,12 @@ app.use(express.json());
 
 app.get('/api/votes', votesHandler);
 app.post('/api/vote', voteHandler);
+
+app.get('/debug/check-file', (req, res) => {
+  const filePath = path.join(process.cwd(), 'service-account.json');
+  const exists = fs.existsSync(filePath);
+  res.status(200).json({ found: exists });
+});
 
 // ❌ Do not use app.listen()
 // ✅ Instead, export the handler
